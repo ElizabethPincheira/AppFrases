fd2b4ed2a1c657956ad65048f32ab8a1a235c628
elizabeth <elizabeth.pincheira.caro@gmail.com>
2025-01-02 15:16:07 -0300

    se vincula componente navbar a las pages

--- src/app/paginas/configuraciones/configuraciones.page.ts
+++ src/app/paginas/configuraciones/configuraciones.page.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {NavbarComponent} from 'src/app/componentes/navbar/navbar.component'
 
 @Component({
   selector: 'app-configuraciones',
   templateUrl: './configuraciones.page.html',
   styleUrls: ['./configuraciones.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, NavbarComponent]
 })
 export class ConfiguracionesPage implements OnInit {
 
--- src/app/paginas/gestion-citas/gestion-citas.page.ts
+++ src/app/paginas/gestion-citas/gestion-citas.page.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { NavbarComponent } from 'src/app/componentes/navbar/navbar.component'
 
 @Component({
   selector: 'app-gestion-citas',
   templateUrl: './gestion-citas.page.html',
   styleUrls: ['./gestion-citas.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, NavbarComponent]
 })
 export class GestionCitasPage implements OnInit {
 
