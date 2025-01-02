ac2680589b767b7a23292cd29579497a79616c74
elizabeth <elizabeth.pincheira.caro@gmail.com>
2025-01-02 14:30:00 -0300

    se crea carpeta componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,19 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    loadComponent: () => import('./paginas/home/home.page').then((m) => m.HomePage),
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
+  },
+  {
+    path: 'gestion-citas',
+    loadComponent: () => import('./paginas/gestion-citas/gestion-citas.page').then( m => m.GestionCitasPage)
+  },
+  {
+    path: 'configuraciones',
+    loadComponent: () => import('./paginas/configuraciones/configuraciones.page').then( m => m.ConfiguracionesPage)
   },
 ];
--- src/app/componentes/navbar/navbar.component.spec.ts
+++ src/app/componentes/navbar/navbar.component.spec.ts
@@ -1,19 +1,19 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-import { GestionCitasComponent } from './gestion-citas.component';
+import { NavbarComponent } from './navbar.component';
 
-describe('GestionCitasComponent', () => {
-  let component: GestionCitasComponent;
-  let fixture: ComponentFixture<GestionCitasComponent>;
+describe('NavbarComponent', () => {
+  let component: NavbarComponent;
+  let fixture: ComponentFixture<NavbarComponent>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ GestionCitasComponent ],
+      declarations: [ NavbarComponent ],
       imports: [IonicModule.forRoot()]
     }).compileComponents();
 
-    fixture = TestBed.createComponent(GestionCitasComponent);
+    fixture = TestBed.createComponent(NavbarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
