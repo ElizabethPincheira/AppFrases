dedc65b9edeb1e42aa34fe68cbfeacf1744baac6
elizabeth <elizabeth.pincheira.caro@gmail.com>
2025-01-02 13:28:12 -0300

    Add configuracion component and gestion citas component

---
+++ src/app/pantallas/gestion-citas/gestion-citas.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-citas',
+  templateUrl: './gestion-citas.component.html',
+  styleUrls: ['./gestion-citas.component.scss'],
+})
+export class GestionCitasComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
